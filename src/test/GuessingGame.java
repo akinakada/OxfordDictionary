ceca1a8c042fa946c090e44b0eb1ea473d263a8a
kjergens <kjergens@users.noreply.github.com>
2018-04-03 10:49:49 -0400

    With teacher comments.

--- src/test/GuessingGame.java
+++ src/test/GuessingGame.java
@@ -15,12 +15,13 @@ public class GuessingGame {
         String randchar = randWord.charAt(0) + blank;
         String L1 = randchar.toUpperCase();
 
-        Dictionary.getRandomWord();
+        Dictionary.getRandomWord();  // These words aren't saved, i.e. these two lines don't do anything. :-)
 
         Dictionary.getRandomWord();
 
         System.out.println("Guess the full word!");
 
+        // Not sure this contributes to your game...leftovers from password maker??
         if (randWord.length() + randWord1.length() >= 8) {
 
             randWord = randWord.replace("a", "4");
@@ -45,10 +46,10 @@ public class GuessingGame {
 
             System.out.println("You guessed it!");
         }
-            if (!userInput.equals(randWord + randWord1)) {
+            if (!userInput.equals(randWord + randWord1)) {  // indenting
 
             System.out.println("Wrong Try Again");
-            Dictionary.getRandomWord();
+            Dictionary.getRandomWord();  // Again, these words aren't saved.
 
             Dictionary.getRandomWord();
 
